1bd06848493070bbc5571a8498949b0a3bbad03f
mcvincekova <martina.cvincekova@wandera.com>
2023-01-26 20:39:11 +0100

    kod z tretej lekcie

--- python_1/seminar3/json1.py
+++ python_1/seminar3/json1.py
@@ -5,12 +5,23 @@ import json
 response = requests.get('https://api.kodim.cz/python-data/people')
 data = json.loads(response.text)
 
+number_of_people = len(data)
+print(f"V zozname je {number_of_people} ludi.")
 
-print(len(data))
 print(data[0])
 
-males = [person for person in data if person["gender"] == "Male"]
-females = [person for person in data if person["gender"] == "Female"]
+women_count = 0
+male_count = 0
+for item in data:
+    if item['gender'] == "Female":
+        women_count += 1
+    else:
+        male_count += 1
 
-print(len(males))
-print(len(females))
+print(f"V zozname je {women_count} zien.")
+print(f"V zozname je {male_count} muzov.")
+
+women = [item for item in data if item['gender'] == "Female"]
+women_count_2 = len(women)
+
+print(women_count_2)
--- python_1/seminar3/json2.py
+++ python_1/seminar3/json2.py
@@ -2,6 +2,17 @@ import requests
 import json
 
 
-response = requests.get('https://svatky.adresa.info/json')
+url = "https://svatky.adresa.info/json?date="
+date = input("Zadaj datum vo formate DDMM: ")
+
+url_with_date = url + date
+print(f"Url: {url_with_date}")
+
+
+response = requests.get(url_with_date)
 data = json.loads(response.text)
-print(data)
+
+# [{'date': '0307', 'name': 'Radomír'}]
+name = data[0]['name']
+
+print(f"Meno: {name}")
